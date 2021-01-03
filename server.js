8be67b9767322a46062c9074da8ddfff91c4c26f
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-01-03 21:41:45 +0900

    backend

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+
+// Middlewares
+
+
+// DB config
+
+// API Endpoints
+app.get('/', (res, req) => 
+  res.status(200).send("Hello World")
+);
+
+// Listener
+app.listen(port, () => 
+  console.log(`listening on localhost: ${port}`)
+);
